425c2821001c9af61cd2160b2dfe19323165bd16
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-08-14 19:38:22 +0200

    Add basic router-vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    MyComponent
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div>Login Page</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
