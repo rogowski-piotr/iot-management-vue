22c49f9e191de8517ee5e00be4d667c24be5d4f6
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-08-20 18:17:45 +0200

    refactor userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -16,15 +16,10 @@ function login(name, password) {
     return fetch(`http://localhost:8080/login`, requestOptions)
         .then(handleResponse)
         .then(user => {
-            console.log(user);
-            // login successful if there's a user in the response
             if (user) {
-                // store user details and basic auth credentials in local storage 
-                // to keep user logged in between page refreshes
                 user.authdata = window.btoa(name + ':' + password);
                 localStorage.setItem('user', JSON.stringify(user));
             }
-
             return user;
         });
 }
@@ -41,26 +36,21 @@ function getAll() {
         host: 'http://localhost:80',
         headers: authHeader()
     };
-
-    console.log(requestOptions)
-
     return fetch(`http://localhost:8080/api_auth/users`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
+
         if (!response.ok) {
             if (response.status === 401) {
-                // auto logout if 401 response returned from api
                 logout();
                 location.reload(true);
             }
-
             const error = (data && data.message) || response.statusText;
             return Promise.reject(error);
         }
-
         return data;
     });
 }
