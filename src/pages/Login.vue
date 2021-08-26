437c874471cda94c5b9b9ac2213055cf2184d6ba
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-08-26 21:53:34 +0200

    fix login notifications

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,39 +1,33 @@
 <template>
-<div class="alert alert-warning" v-show="responseStatus === 401" role="alert" id="alert-unauthorized">
-    <h4 class="alert-heading">Incorrect login details!</h4>
-</div>
-<div class="alert alert-warning" v-show="responseStatus === 400" role="alert" id="alert-incorrect-data">
-    <h4 class="alert-heading">Incorrect login details!</h4>
-    <hr>
-    <p class="mb-0">Please complete all fields</p>
-</div>
-<div class="alert alert-warning" v-show="responseStatus != null && responseStatus != 200 && responseStatus != 400 && responseStatus != 401" role="alert" id="alert-unexpected">
-    <h4 class="alert-heading">Occurred unexpected error</h4>
-    <hr>
-    <p class="mb-0">Try again later</p>
-</div>
-<div class="row">
-    <div class="col-md-6 offset-md-3">
-        <div>
-            <div>
-                <h3>Login</h3>
-                <hr/>
-            </div>
-            <div class="form-group">
-                <label>Email or Username</label>
-                <input type="text" class="form-control" v-model="name"/>
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control" v-model="password"/>
-            </div>
-            <div class="my-3">
-                <button @click="validateAuth" type="submit" class="btn btn-primary">Login</button>
-            </div>
-            <a href="">forgot password?</a> or <a href="/signup">signup</a>
+    <div class="alert alert-warning" v-show="errorMessage != null" role="alert" id="alert-incorrect-data">
+        <h4 class="alert-heading">Incorrect login details!</h4>
+        <div v-for="msg in errorMessage" :key="msg.id">
+            <hr>
+            {{ msg }}
         </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-md-4 offset-md-4">
+            <div>
+                <div>
+                    <h3>Login</h3>
+                    <hr/>
+                </div>
+                <div class="form-group">
+                    <label>Email or Username</label>
+                    <input type="text" class="form-control" v-model="name"/>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" class="form-control" v-model="password"/>
+                </div>
+                <div class="my-3">
+                    <button @click="validateAuth" type="submit" class="btn btn-primary">Login</button>
+                </div>
+                <a href="">forgot password?</a> or <a href="/signup">signup</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -46,7 +40,7 @@ export default {
     return {
         name: null,
         password: null,
-        responseStatus: null,
+        errorMessage: null,
     }
   },
   created () {
@@ -55,7 +49,17 @@ export default {
   methods: {
     validateAuth() {
         userService.login(this.name, this.password)
-                .then(router.push("/"));
+                .then(router.push("/"))
+                .catch(errorMsg => this.errorMessage = this.parseErrorMsg(errorMsg));
+    },
+    parseErrorMsg(errorMsg) {
+        var msg = [];
+        var keys = Object.keys(errorMsg);
+        keys.forEach(function(key) {
+            msg.push(key + ' ' + errorMsg[key]);
+        });
+        console.log(msg);
+        return msg;
     },
   }
 }
