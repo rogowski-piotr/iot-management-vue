d292b1574520c592834f2f37d3256a1dc4e39af7
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-09-04 14:46:04 +0200

    add service for place

---
+++ src/services/placeService.js
@@ -0,0 +1,31 @@
+import { authHeader } from '../helpers/auth-header';
+import { userService } from '.';
+
+export const placeService = {
+    getAll
+};
+
+function getAll() {
+    console.log('get all')
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+    return fetch(`http://192.168.0.102:8080/api_auth/places`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                userService.logout();
+                location.reload(true);
+            }
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        return data;
+    });
+}
