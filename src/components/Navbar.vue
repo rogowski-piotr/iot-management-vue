04f831140130e6923fb9f498498c0fbd6e7601c9
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-09-06 20:34:55 +0200

    change top navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,18 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="px-4">
-            <a class="navbar-brand" href="#">IoT Management</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item" v-bind:class = "{ active: (this.currentElement==='home') }">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item" v-bind:class = "{ active: (this.currentElement==='sensors') }">
-                    <a class="nav-link" href="/sensors">Sensors</a>
-                </li>
-                <li class="nav-item" v-bind:class = "{ active: (this.currentElement==='places') }">
-                    <a class="nav-link" href="/places">Places</a>
-                </li>
-            </ul>
-            <span class="navbar-text">
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{user.username}}</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Users</a>
-                        <a class="dropdown-item" href="#">Settings</a>
-                        <div class="dropdown-divider"></div>
-                        <div class="dropdown-item">
-                            <router-link to="/login">Logout</router-link>
-                        </div>
+    <nav class="navbar navbar-light sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a style="color: white" class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">IoT Management</a>
+        <span class="navbar-text">
+            <div class="nav-item dropdown">
+                <a style="color: white" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{user.username}}</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Settings</a>
+                    <div class="dropdown-divider"></div>
+                    <div class="dropdown-item">
+                        <router-link to="/login">Logout</router-link>
                     </div>
                 </div>
-            </span>
-        </div>
+            </div>
+        </span>
     </nav>
 </template>
 
@@ -43,9 +23,6 @@ export default {
         return {
             user: {}
         }
-    },
-    props: {
-        currentElement: String
     },
     created () {
       this.user = JSON.parse(localStorage.getItem('user'));
